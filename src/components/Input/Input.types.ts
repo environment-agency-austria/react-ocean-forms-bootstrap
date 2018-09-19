cfdab4c24474a24e1fda82f6db27b9925296bdbe
Denis Masinovic <denis.masinovic@gmail.com>
2018-09-19 22:40:35 +0200

    Use new IInfoProps

--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -1,12 +1,15 @@
 import { IFieldComponentProps } from 'react-ocean-forms';
 // tslint:disable-next-line:no-submodule-imports
 import { InputType } from 'reactstrap/lib/Input';
+import { IInfoProps } from '../FieldLine';
 
 /**
  * Props for the Input component
  */
-export interface IInputProps extends IFieldComponentProps {
+export interface IInputProps extends IFieldComponentProps, IInfoProps {
+  /**
+   * Html input type
+   */
   // tslint:disable-next-line:no-reserved-keywords
   type: InputType;
-  info?: string;
 }
