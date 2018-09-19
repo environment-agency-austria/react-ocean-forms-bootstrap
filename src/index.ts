afe1be6b56ac24bbed87692d4fdf4a3b75b107f4
Denis Masinovic <denis.masinovic@gmail.com>
2018-09-20 00:15:16 +0200

    Migrated boilerplate code to typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Register our custom messages to react-ocean-forms
+import './utils/registerCustomMessages';
+
+export * from './components/Check';
+export * from './components/DatePicker';
+export * from './components/FieldLine/FieldError';
+export * from './components/FieldLine';
+// TODO: Re-Add fixed FileInput
+// export { default as FileInput } from './components/FileInput';
+export * from './components/Input';
+export * from './components/Select';
+export * from './components/ValidationSummary';
+export * from './components/FormButton';
+export * from './components/OnOffToggleButton';
